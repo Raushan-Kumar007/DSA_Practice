91c05749e6d085ee08e6cab1d65940ea21619012
Rausha Kumar <ravisankaranand199@gmail.com>
2022-10-02 23:19:14 +0530

    Linear Search

---
+++ src/Array/LinearSearch.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int item = sc.nextInt();
+        System.out.print(linearSearch(arr,item));
+    }
+}
