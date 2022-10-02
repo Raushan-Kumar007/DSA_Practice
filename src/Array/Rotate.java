9d65dee5bb25eb08268103cf3ecc050036e10b58
Rausha Kumar <ravisankaranand199@gmail.com>
2022-10-03 00:20:27 +0530

    rotate Array

---
+++ src/Array/Rotate.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Rotate {
+    public static void RotateArray(int[] arr, int k) {
+        int n = arr.length;
+        k = k % n;
+        while (k > 0) {
+            int temp = arr[n - 1];
+            for (int i = n - 2; i >= 0; i--) {
+                arr[i + 1] = arr[i];
+
+            }
+            arr[0] = temp;
+            k--;
+        }
+    }
+        public static void main(String[] args) {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 3;
+            RotateArray(arr, k);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+    }
+}
