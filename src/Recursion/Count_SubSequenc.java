0f9730bddeac2b8c09a5c8b20de95aab465b8c34
Rausha Kumar <ravisankaranand199@gmail.com>
2022-10-28 00:05:54 +0530

    count sub sequence of string using recursion

--- src/Recursion/Count_SubSequenc.java
+++ src/Recursion/Count_SubSequenc.java
@@ -1,20 +1,31 @@
 package Recursion;
 
 public class Count_SubSequenc {
-    static int count = 0;
-    static void printSubsequence(String que, String ans){
-        if(que.length()==0){
-            System.out.println(ans);
-            count++;
-            return;
+//    static int count = 0;
+//    static void printSubsequence(String que, String ans){
+//        if(que.length()==0){
+//            System.out.println(ans);
+//            count++;
+//            return;
+//        }
+//       char ch =  que.charAt(0);
+//        printSubsequence(que.substring(1),ans);
+//        printSubsequence(que.substring(1),ans+ch);
+//    }
+    // by using recursion
+    static int countAns(String ques, String ans){
+        if(ques.length()==0){
+            return 1;
         }
-       char ch =  que.charAt(0);
-        printSubsequence(que.substring(1),ans);
-        printSubsequence(que.substring(1),ans+ch);
+        char ch = ques.charAt(0);
+        int a = countAns(ques.substring(1),ans);
+        int b = countAns(ques.substring(1),ans+ch);
+        return a+b;
     }
     public static void main(String[] args) {
         String str = "abc";
-        printSubsequence(str,"");
-        System.out.println(count);
+ //       printSubsequence(str,"");
+ //       System.out.println(count);
+        System.out.println(countAns(str,""));
     }
 }
