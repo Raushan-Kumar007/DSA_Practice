7bd931886725fed7a8a155ff165e5d34d78b18ec
Rausha Kumar <ravisankaranand199@gmail.com>
2022-12-30 11:26:02 +0530

    main method to implement stack

--- src/Stack/Stacks_implement.java
+++ src/Stack/Stacks_implement.java
@@ -1,2 +1,21 @@
-package Stack;public class Stacks_implement {
+package Stack;
+
+
+
+public class Stacks_implement {
+    public static void main(String[] args) throws Exception {
+        Stacks s = new Stacks();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        System.out.println(s.peek());
+        s.display();
+        System.out.println(s.pop());
+        s.display();
+        System.out.println(s.size());
+        s.push(-9);
+        s.display();
+    }
 }
