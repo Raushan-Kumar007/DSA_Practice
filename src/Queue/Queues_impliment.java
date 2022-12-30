069bf0add1b759d226602e0be093409d5147f2d9
Rausha Kumar <ravisankaranand199@gmail.com>
2022-12-30 12:28:09 +0530

    queue implimentation using array

--- src/Queue/Queues_impliment.java
+++ src/Queue/Queues_impliment.java
@@ -1,2 +1,17 @@
-package Queue;public class Queues_impliment {
+package Queue;
+
+public class Queues_impliment {
+    public static void main(String[] args) throws Exception {
+        Queues q = new Queues();
+        q.EnQueue(10);
+        q.EnQueue(20);
+        q.EnQueue(30);
+        q.EnQueue(40);
+        System.out.println(q.DeQueue());
+        System.out.println(q.DeQueue());
+        q.EnQueue(-10);
+        q.EnQueue(-20);
+        q.EnQueue(-30);
+        q.display();
+    }
 }
