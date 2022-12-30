a1799ab15f98a8fd286f47764be0abdbe0df5b0d
Rausha Kumar <ravisankaranand199@gmail.com>
2022-12-30 12:26:59 +0530

    queue implimentation using arrays

---
+++ src/Queue/Queues.java
@@ -0,0 +1,57 @@
+package Queue;
+
+public class Queues {
+    private int[] arr;
+    private int size;
+    private int front;
+    public Queues(){
+     this.arr=new int[5];
+     this.size=0;
+     this.front=0;
+    }
+    public Queues(int item){
+      this.arr=new int[item];
+      this.size=0;
+      this.front=0;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public  boolean isFull(){
+       return size==arr.length;
+    }
+    public int size(){
+        return size;
+    }
+    public void EnQueue(int item) throws Exception {
+        if(isFull()==true){
+            throw new Exception("queue full hai");
+        }
+        int idx = (front+size)%arr.length;
+        arr[idx]=item;
+        size++;
+    }
+    public int DeQueue() throws Exception {
+        if(isEmpty()==true){
+            throw new Exception("queue full hai");
+        }
+     int val = arr[front];
+     front = (front + 1) % arr.length; // % is for making circular queue
+     size--;
+     return val;
+    }
+    public int  GetFront(int item) throws Exception {
+        if(isEmpty()==true){
+            throw new Exception("queue full hai");
+        }
+        int val = arr[front];
+        return val;
+    }
+    public void display(){
+        for (int i = 0; i < size; i++) {
+           int idx = (front + i) % arr.length; // % is for making circular queue
+            System.out.print(arr[idx]+" ");
+        }
+        System.out.println();
+    }
+}
