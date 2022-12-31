38734543896e1c593ed846094aaa2ae265443118
Rausha Kumar <ravisankaranand199@gmail.com>
2022-12-31 18:38:02 +0530

    main method to impliment linkedlist

--- src/linkedlist/linkedList_implement.java
+++ src/linkedlist/linkedList_implement.java
@@ -1,2 +1,18 @@
-package linkedlist;public class linkedList_implement {
+package linkedlist;
+
+public class linkedList_implement {
+    public static void main(String[] args) throws Exception {
+        linkedList ll = new linkedList();
+        ll.addfirst(10);
+        ll.addfirst(20);
+        ll.addfirst(30);
+        ll.Display();
+        ll.addlast(40);
+        ll.Display();
+        ll.addatIndex(2,-9);
+        ll.Display();
+        System.out.println(ll.getFirst());
+        System.out.println(ll.getLast());
+        System.out.println(ll.getatIndex(2));
+    }
 }
