e372b1a491e078cdda69440f71101cd10f3b3463
Rausha Kumar <ravisankaranand199@gmail.com>
2023-01-02 17:04:21 +0530

    dynamic Queues implementation

--- src/dynamicStacks/dynamic_Queues.java
+++ src/dynamicStacks/dynamic_Queues.java
@@ -1,2 +1,19 @@
-package dynamicStacks;public class dynamic_Queues {
+package dynamicStacks;
+
+import Queue.Queues;
+
+public class dynamic_Queues extends Queues {
+    @Override
+    public void EnQueue(int item) throws Exception {
+       if(isFull()){
+           int[] newArr = new int[2*arr.length];
+           for (int i = 0; i < size; i++) {
+               int idx = (front + i) % arr.length; // % is for making circular queue
+               newArr[i] = arr[idx];
+           }
+           arr = newArr;
+           front=0;
+       }
+       super.EnQueue(item);
+    }
 }
