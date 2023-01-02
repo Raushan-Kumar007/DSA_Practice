d279fbe81be0fb692598e48bfb1527bef696b273
Rausha Kumar <ravisankaranand199@gmail.com>
2023-01-02 16:41:02 +0530

    implement main method of dynamic stack

--- src/dynamicStacks/implementMain.java
+++ src/dynamicStacks/implementMain.java
@@ -1,2 +1,17 @@
-package dynamicStacks;public class implementMain {
+package dynamicStacks;
+
+public class implementMain {
+    public static void main(String[] args) throws Exception {
+        dynamic_Stacks ds = new dynamic_Stacks();
+        ds.push(10);
+        ds.push(20);
+        ds.push(30);
+        ds.push(40);
+        ds.push(50);
+        ds.push(60);
+        ds.push(70);
+        ds.push(80);
+        ds.push(90);
+        ds.display();
+    }
 }
