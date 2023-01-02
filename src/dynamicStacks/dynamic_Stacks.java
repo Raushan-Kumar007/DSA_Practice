13f66cfd5a6a228f0174b5edd11be19691439bed
Rausha Kumar <ravisankaranand199@gmail.com>
2023-01-02 16:40:00 +0530

    implement dynamic stacks

--- src/dynamicStacks/dynamic_Stacks.java
+++ src/dynamicStacks/dynamic_Stacks.java
@@ -1,2 +1,17 @@
-package dynamicStacks;public class dynamic_Stacks {
+package dynamicStacks;
+
+import Stack.Stacks;
+
+public class dynamic_Stacks extends Stacks {
+    @Override
+    public void push(int item) throws Exception {
+      if(this.isFull()==true){
+          int[] newArr = new int[2*arr.length];
+          for(int i=0; i<super.arr.length;i++){
+              newArr[i] = arr[i];
+          }
+          arr = newArr;
+      }
+      super.push(item);
+   }
 }
