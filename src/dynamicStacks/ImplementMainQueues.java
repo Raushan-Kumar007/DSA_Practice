e6955569b6b6f804b0d1f3070e32447d25a43b8a
Rausha Kumar <ravisankaranand199@gmail.com>
2023-01-02 17:05:32 +0530

    main method of dynamic implimentation

--- src/dynamicStacks/ImplementMainQueues.java
+++ src/dynamicStacks/ImplementMainQueues.java
@@ -1,2 +1,13 @@
-package dynamicStacks;public class ImplementMainQueues {
+package dynamicStacks;
+
+public class ImplementMainQueues {
+    public static void main(String[] args) throws Exception {
+        dynamic_Queues dq = new dynamic_Queues();
+        dq.EnQueue(10);
+        dq.EnQueue(20);
+        dq.EnQueue(30);
+        dq.display();
+        dq.DeQueue();
+        dq.display();
+    }
 }
