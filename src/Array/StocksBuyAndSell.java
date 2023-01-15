8c27f4b9ac924d1f16be0e8600b64edfd0909885
Rausha Kumar <ravisankaranand199@gmail.com>
2023-01-15 19:47:52 +0530

    efficient approch to find profit to sell or buy stocks

--- src/Array/StocksBuyAndSell.java
+++ src/Array/StocksBuyAndSell.java
@@ -1,22 +1,33 @@
 package Array;
 
 public class StocksBuyAndSell {
-    static int findProfit(int[] price,int start, int end){
+//    static int findProfit(int[] price,int start, int end){
+//        int n = price.length;
+//        int profit = 0;
+//        if(end <= start)
+//            return 0;
+//        for(int i=start;i<end;i++){
+//            for(int j=i;j<=end;j++){
+//                int curr_profit = price[j]-price[i]+findProfit(price,start,i-1)+
+//                        findProfit(price,j+1,end);
+//                profit=Math.max(profit,curr_profit);
+//            }
+//        }
+//        return profit;
+//    }
+    static int findProfit(int[] price){
         int n = price.length;
         int profit = 0;
-        if(end <= start)
-            return 0;
-        for(int i=start;i<end;i++){
-            for(int j=i;j<=end;j++){
-                int curr_profit = price[j]-price[i]+findProfit(price,start,i-1)+
-                        findProfit(price,j+1,end);
-                profit=Math.max(profit,curr_profit);
+        for(int i=1;i<n;i++){
+            if(price[i]>price[i-1]){
+                profit += (price[i]-price[i-1]);
             }
         }
         return profit;
     }
     public static void main(String[] args) {
         int arr[] = {1,5,3,8,12};
-        System.out.println(findProfit(arr,0,arr.length-1));
+       // System.out.println(findProfit(arr,0,arr.length-1));
+        System.out.println(findProfit(arr));
     }
 }
