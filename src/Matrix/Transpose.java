fb9c7a171a255fc2247ef008c9763b282e042993
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-22 02:52:21 +0530

    efficient approch of transpose the matrix

--- src/Matrix/Transpose.java
+++ src/Matrix/Transpose.java
@@ -2,15 +2,24 @@ package Matrix;
 
 public class Transpose {
     static void solve(int[][] arr, int n, int m){
-        int[][] temp = new int[n][m];
+//        int[][] temp = new int[n][m];
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                temp[i][j]=arr[j][i];
+//            }
+//        }
+//        for(int i=0;i<n;i++){
+//            for(int j=0;j<m;j++){
+//                arr[i][j]=temp[i][j];
+//            }
+//        }
+//       Diagonal element is always same if we transpose the matrix here we are sawping the i+1 element
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                temp[i][j]=arr[j][i];
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[i][j]=temp[i][j];
+            for(int j=i+1;j<m;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
     }
