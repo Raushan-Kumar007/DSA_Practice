0e4390a796c05dd1c331aa284caade39b8cb40e5
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-23 00:46:04 +0530

    spiral matrix

---
+++ src/Matrix/spiralMatrix.java
@@ -0,0 +1,32 @@
+package Matrix;
+
+public class spiralMatrix {
+    static void solve(int[][] arr, int r,int c){
+        int top=0,left=0,right=r-1,bottom=c-1;
+        while (top<=bottom && left<=right){
+            for(int i=left;i<=right;i++)
+                System.out.print(arr[top][i]+" ");
+                top++;
+
+            for(int i=top;i<=bottom;i++)
+                System.out.print(arr[i][right]+" ");
+                right--;
+
+            if(top<=bottom){
+                for(int i=right;i>=left;i--)
+                    System.out.print(arr[bottom][i]+" ");
+                    bottom--;
+
+            }
+            if(left <= right){
+                for(int i=bottom; i>= top; i--)
+                    System.out.print(arr[i][left]+" ");
+                    left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+      int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+      solve(arr,4,4);
+    }
+}
