2edfc6c5a214d4f7df51db28f53fd72bd59717a9
Rausha Kumar <ravisankaranand199@gmail.com>
2023-02-23 01:23:04 +0530

    Find elememt in shorted matrix

--- src/Matrix/FindEleInShortedMat.java
+++ src/Matrix/FindEleInShortedMat.java
@@ -2,14 +2,27 @@ package Matrix;
 
 public class FindEleInShortedMat {
     static void solve(int[][] arr,int r, int c,int x){
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(arr[i][j]==x){
-                    System.out.print(i+","+j);
-                    return;
-                }
-            }
-        }
+//        for(int i=0;i<r;i++){
+//            for(int j=0;j<c;j++){
+//                if(arr[i][j]==x){
+//                    System.out.print(i+","+j);
+//                    return;
+//                }
+//            }
+//        }
+//        System.out.print("Not Found");
+//    start comparing from top right or bottom left corner
+         int low = 0; int high = c-1;
+         while (low<high && high>=0){
+             if(arr[low][high]==x){
+                 System.out.print(low+","+high);
+                 return;
+             }else if(arr[low][high]>x){
+                 high--;
+             }else {
+                 low++;
+             }
+         }
         System.out.print("Not Found");
     }
     public static void main(String[] args) {
